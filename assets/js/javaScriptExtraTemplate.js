52e77578920e45cd3567514ccf31a596cbe6a304
Artur Pedroso <apedroso@student.dei.uc.pt>
2015-11-20 19:11:40 +0000

    Repair error in form. Now users have to fill all the form to be able to send it

--- assets/js/javaScriptExtraTemplate.js
+++ assets/js/javaScriptExtraTemplate.js
@@ -5,6 +5,25 @@ $signupForm.submit(function(e) {
 	$signupForm.find('.alert--loading').remove();
 	$signupForm.find('.alert--success').remove();
 	$signupForm.find('.alert--error').remove();
+	var data = $(this).serializeArray();
+	var counter = 0;
+	for (key in data){
+		counter++;
+		if(data[key]){
+			if(data[key].value.trim() == ''){
+				$signupForm.append("<div class='alert alert--error'>Please fill all the information and try again. Thanks!</div>");
+				return;
+			}
+		}
+		else{
+			$signupForm.append("<div class='alert alert--error'>Please fill all the information and try again. Thanks!</div>");
+			return;
+		}
+	}
+	if (counter != 3){
+		$signupForm.append("<div class='alert alert--error'>Please fill all the information and try again. Thanks!</div>");
+		return;
+	}
 	$.ajax({
 		url: '//formspree.io/poofsquest.forms@gmail.com',
 		method: 'POST',
@@ -14,15 +33,12 @@ $signupForm.submit(function(e) {
 			$signupForm.append('<div class="alert alert--loading">Processing request…</div>');
 		},
 		success: function(data) {
-			$signupForm.find('#demo-name').val("Name");
-			$signupForm.find('#demo-email').val("Email Address");
-			$signupForm.find('#demo-message').val("Enter your message");
 			$signupForm.find('.alert--loading').hide();
 			$signupForm.append('<div class="alert alert--success">Thanks! We just got your email.</div>');
 		},
 		error: function(err) {
 			$signupForm.find('.alert--loading').hide();
-			$signupForm.append('<div class="alert alert--error">Ops, there was an error. Try again!</div>');
+			$signupForm.append('<div class="alert alert--error">Ops, there was an error. Please try again!</div>');
 		}
 	});
 });
